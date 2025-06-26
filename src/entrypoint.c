a053fb2d896e2f2465c5e4b99fb40e8f971b91c6
BrftM <moritz.broft@student.uibk.ac.at>
2025-06-26 14:56:35 +0200

    Update-config

--- src/entrypoint.c
+++ src/entrypoint.c
@@ -1,8 +1,8 @@
 // We need to forward routine registration from C to Rust
 // to avoid the linker removing the static library.
 
-void R_init_fqtk_wrapper_extendr(void *dll);
+void R_init_fqtkWrapper_extendr(void *dll);
 
-void R_init_fqtk_wrapper(void *dll) {
-    R_init_fqtk_wrapper_extendr(dll);
+void R_init_fqtkWrapper(void *dll) {
+    R_init_fqtkWrapper_extendr(dll);
 }
