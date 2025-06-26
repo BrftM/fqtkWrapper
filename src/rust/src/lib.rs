74c3c65ccbc98ceb36fa55cc93e37a32e8798310
BrftM <moritz.broft@student.uibk.ac.at>
2025-06-26 16:30:11 +0200

    update-comments

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,38 +1,31 @@
 use extendr_api::prelude::*;
 use std::process::{Command};
 
-/// Function to call the fqtk CLI command for demultiplexing.
-/// Exposes the demux functionality as a Rust function that can be called from R.
 /// @export
 #[extendr]
 fn fqtk_demux(
-    inputs: Vec<String>,               // List of input files
-    max_mismatches: usize,             // Max mismatches allowed
-    read_structures: Vec<String>,      // List of read structures
-    sample_metadata: String,           // Path to sample metadata file
-    output: String                     // Output directory
+    inputs: Vec<String>,              
+    max_mismatches: usize,            
+    read_structures: Vec<String>,      
+    sample_metadata: String,           
+    output: String                    
 ) -> String {
 
-    // Build the `fqtk demux` command using the provided parameters
     let mut command = Command::new("fqtk");
     command.arg("demux");
 
-    // Add inputs (path to input files)
     for input in inputs {
         command.arg("--inputs").arg(input);
     }
 
-    // Add read structures
     for read_structure in read_structures {
         command.arg("--read-structures").arg(read_structure);
     }
 
-    // Add other parameters
     command.arg("--sample-metadata").arg(sample_metadata)
            .arg("--output").arg(output)
            .arg("--max-mismatches").arg(max_mismatches.to_string());
 
-    // Execute the command
     match command.output() {
         Ok(output) => {
             if output.status.success() {
@@ -46,9 +39,6 @@ fn fqtk_demux(
     }
 }
 
-// Macro to generate exports.
-// This ensures exported functions are registered with R.
-// See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod fqtkWrapper;
     fn fqtk_demux;
