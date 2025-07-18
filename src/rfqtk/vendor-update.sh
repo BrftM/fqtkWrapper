f7bc0ca24211543a7985d1e1f1407dbd0820ad80
BrftM <moritz.broft@student.uibk.ac.at>
2025-07-18 09:40:04 +0200

    Update-vendor-script

--- src/rfqtk/vendor-update.sh
+++ src/rfqtk/vendor-update.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
-rm -Rf vendor vendor.tar.xz
-cargo vendor
-tar -cJ --no-xattrs -f vendor.tar.xz vendor
-rm -Rf vendor
+#!/bin/bash
+
+set -e
+
+# Clean previous files
+rm -rf vendor vendor.tar.xz fqtk-bin
+
+# Build fqtk statically and locally
+cargo install --locked --root ./fqtk-bin fqtk
+
+# Copy only the binary
+mkdir -p vendor
+cp fqtk-bin/bin/fqtk vendor/
+
+# Package it
+tar -cJf vendor.tar.xz vendor
+
+# Clean up (optional, CI might not need it)
+rm -rf vendor fqtk-bin
